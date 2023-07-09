bf2127e91c677d6efa61f1921e1b2055bd464a42
AlexFZR <abadragan@gmail.com>
2023-07-09 15:54:50 +0200

    cross site scripting

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"os"
 )
 
 type User struct {
 	Name string
-	Age  int
-	Meta UserMeta
-}
-
-type UserMeta struct {
-	Visits int
+	Bio  string
 }
 
 func main() {
@@ -22,21 +16,10 @@ func main() {
 		panic(err)
 	}
 
-	// user := struct {
-	// 	Name string
-	// }{
-	// 	Name: "John Doe",
-	// }
-
 	user := User{
 		Name: "John Doe",
-		Age:  69,
-		Meta: UserMeta{
-			Visits: 42,
-		},
+		Bio:  `<script>alert("This is a bio")</script>`,
 	}
-
-	fmt.Println(user.Meta.Visits)
 
 	err = t.Execute(os.Stdout, user)
 	if err != nil {
