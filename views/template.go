bc447f45ceb76ac68bd00f6af637db83522fd08b
AlexFZR <abadragan@gmail.com>
2023-07-10 20:06:03 +0200

    must function

--- views/template.go
+++ views/template.go
@@ -7,6 +7,13 @@ import (
 	"net/http"
 )
 
+func Must(tpl Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return tpl
+}
+
 func Parse(filepath string) (Template, error) {
 	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
