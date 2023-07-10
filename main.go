402c22237fbf580a2e23b8e33d801dcdf823f3d5
AlexFZR <abadragan@gmail.com>
2023-07-10 18:13:54 +0200

    views package

--- main.go
+++ main.go
@@ -2,28 +2,22 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
+	"github.com/AlexTLDR/WebDev/views"
 	"github.com/go-chi/chi/v5"
 )
 
 func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles(filepath)
+	t, err := views.Parse(filepath)
 	if err != nil {
 		log.Printf("parsing template: %v", "err")
 		http.Error(w, "Error parsing template", http.StatusInternalServerError)
 		return
 	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", "err")
-		http.Error(w, "Error executing template", http.StatusInternalServerError)
-		return
-	}
+	t.Execute(w, nil)
 }
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// tpl, err := template.ParseFiles("templates/home.gohtml") -> this is only working in unix systems
