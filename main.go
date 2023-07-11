043093285603728fc5e39bb9c57d5bd58683107f
AlexTLDR <alex.badragan@protonmail.com>
2023-07-11 21:49:00 +0200

    signup form

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ func main() {
 
 	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
+	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 Page Not Found", http.StatusNotFound)
 	})
