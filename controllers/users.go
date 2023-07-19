254ba7daa34109ad044185c0b5f360789b4ccc47
AlexTLDR <alex.badragan@protonmail.com>
2023-07-19 16:11:01 +0200

    custom template functions

--- controllers/users.go
+++ controllers/users.go
@@ -2,12 +2,10 @@ package controllers
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/AlexTLDR/WebDev/models"
-	"github.com/gorilla/csrf"
 )
 
 type Users struct {
@@ -20,11 +18,9 @@ type Users struct {
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	var data struct {
-		Email     string
-		CSRFField template.HTML
+		Email string
 	}
 	data.Email = r.FormValue("email")
-	data.CSRFField = csrf.TemplateField(r)
 	u.Templates.New.Execute(w, data)
 }
 
--- views/template.go
+++ views/template.go
@@ -16,24 +16,34 @@ func Must(tpl Template, err error) Template {
 }
 
 func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
-	tpl, err := template.ParseFS(fs, patterns...)
+	tpl := template.New(patterns[0])
+	tpl = tpl.Funcs(
+		template.FuncMap{
+			"csrfField": func() template.HTML {
+				return `<input type="hidden" />`
+			},
+		},
+	)
+
+	tpl, err := tpl.ParseFS(fs, patterns...)
 	if err != nil {
 		return Template{}, fmt.Errorf("ParseFS template: %w", err)
 	}
+
 	return Template{
 		htmlTpl: tpl,
 	}, nil
 }
 
-func Parse(filepath string) (Template, error) {
-	tpl, err := template.ParseFiles(filepath)
-	if err != nil {
-		return Template{}, fmt.Errorf("parsing template: %w", err)
-	}
-	return Template{
-		htmlTpl: tpl,
-	}, nil
-}
+// func Parse(filepath string) (Template, error) {
+// 	tpl, err := template.ParseFiles(filepath)
+// 	if err != nil {
+// 		return Template{}, fmt.Errorf("parsing template: %w", err)
+// 	}
+// 	return Template{
+// 		htmlTpl: tpl,
+// 	}, nil
+// }
 
 type Template struct {
 	htmlTpl *template.Template
