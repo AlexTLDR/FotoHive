fa0dda1df492103663ce1821d91265f8338774f9
AlexTLDR <alex.badragan@protonmail.com>
2023-07-21 12:53:35 +0200

    query users with session tokens

--- models/session.go
+++ models/session.go
@@ -64,8 +64,25 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 }
 
 func (ss *SessionService) User(token string) (*User, error) {
-	// TODO: Implement SessionService.User
-	return nil, nil
+	tokenHash := ss.hash(token)
+	var user User
+	row := ss.DB.QueryRow(`
+			SELECT user_id 
+			FROM sessions 
+			WHERE token_hash = $1;`, tokenHash)
+	err := row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+
+	row = ss.DB.QueryRow(`
+			SELECT email, password_hash
+			FROM users WHERE id = $1;`, user.ID)
+	err = row.Scan(&user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+	return &user, nil
 }
 
 func (ss *SessionService) hash(token string) string {
