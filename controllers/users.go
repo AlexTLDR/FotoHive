9474fa8b7d02d565a9897e7b120e4bcb6998a500
AlexTLDR <alex.badragan@protonmail.com>
2023-07-21 13:12:44 +0200

    sign out handler

--- controllers/users.go
+++ controllers/users.go
@@ -90,3 +90,19 @@ func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Current user: %s\n", user.Email)
 }
+
+func (u Users) ProcessSignOut(w http.ResponseWriter, r *http.Request) {
+	token, err := readCookie(r, CookieSession)
+	if err != nil {
+		http.Redirect(w, r, "/signin", http.StatusFound)
+		return
+	}
+	err = u.SessionService.Delete(token)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Something went wrong. Please try again later.", http.StatusInternalServerError)
+		return
+	}
+	deleteCookie(w, CookieSession)
+	http.Redirect(w, r, "/signin", http.StatusFound)
+}
