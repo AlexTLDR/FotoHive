02be8b784d21b78267a202855c0bd6fbff0e9961
AlexTLDR <alex.badragan@protonmail.com>
2023-07-26 16:18:59 +0200

    context keys

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,3 +1,5 @@
+// experimenting with context
+
 package main
 
 import (
@@ -5,9 +7,19 @@ import (
 	"fmt"
 )
 
+type key string
+
+const (
+	favoriteColorKey key = "favorite-color"
+)
+
 func main() {
 	ctx := context.Background()
-	ctx = context.WithValue(ctx, "favorite-color", "dark-red")
-	value := ctx.Value("favorite-color")
-	fmt.Println(value)
+	ctx = context.WithValue(ctx, favoriteColorKey, "dark-red")
+
+	ctx = context.WithValue(ctx, "favorite-color", 123)
+
+	value := ctx.Value(favoriteColorKey)
+	value2 := ctx.Value("favorite-color")
+	fmt.Println(value, value2)
 }
