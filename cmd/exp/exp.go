c26e69585385acb5712b45821900e489fb809815
AlexTLDR <alex.badragan@protonmail.com>
2023-07-27 16:25:47 +0200

    sending emails with SMTP

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -3,14 +3,23 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/go-mail/mail/v2"
 )
 
+const (
+	// was lazy to use a .env file but this is a sandbox account and I have regenerated the credentials
+	host     = "sandbox.smtp.mailtrap.io"
+	port     = 2525
+	username = "ef98e50d183d83"
+	password = "ecc7293fbc4307"
+)
+
 func main() {
 	from := "alex@alex.com"
-	to := "alex@tldr.com"
+	to := "alex.badragan@protonmail.com"
 	subject := "hello"
 	plaintext := "hello world from the email body"
 	html := "<h1>Hello World</h1><p>hello world from the email body</p><p>more text</p>"
@@ -22,4 +31,11 @@ func main() {
 	msg.SetBody("text/plain", plaintext)
 	msg.AddAlternative("text/html", html)
 	msg.WriteTo(os.Stdout)
+
+	dialer := mail.NewDialer(host, port, username, password)
+	err := dialer.DialAndSend(msg)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Email sent!")
 }
