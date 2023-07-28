7fafbeee7f26e2f8e98c25db54352dddb204c743
AlexTLDR <alex.badragan@protonmail.com>
2023-07-28 16:01:17 +0200

    forgot-pw HTML template

--- main.go
+++ main.go
@@ -38,6 +38,9 @@ func main() {
 	}
 
 	// Middleware
+	umw := controllers.UserMiddleware{
+		SessionService: &sessionService,
+	}
 
 	csrfKey := "0An3m5VpO8cnA9LTe60RQcmmyJwhj3f5"
 	csrfMw := csrf.Protect([]byte(csrfKey), csrf.Secure(false)) //TODO: change to true in production
@@ -50,9 +53,7 @@ func main() {
 	}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
-	umw := controllers.UserMiddleware{
-		SessionService: &sessionService,
-	}
+	usersC.Templates.ForgotPassword = views.Must(views.ParseFS(templates.FS, "forgot-pw.gohtml", "tailwind.gohtml"))
 
 	// Routes
 
@@ -67,6 +68,8 @@ func main() {
 	r.Get("/signin", usersC.SignIn)
 	r.Post("/signin", usersC.ProcessSignIn)
 	r.Post("/signout", usersC.ProcessSignOut)
+	r.Get("/forgot-pw", usersC.ForgotPassword)
+	r.Post("/forgot-pw", usersC.ProcessForgotPassword)
 	r.Route("/users/me", func(r chi.Router) {
 		r.Use(umw.RequireUser)
 		r.Get("/", usersC.CurrentUser)
