ac6b6ad29e7e6489df55b90ea5d1053f0344c5bd
AlexTLDR <alex.badragan@protonmail.com>
2023-07-30 14:27:03 +0200

    pw reset creatin

--- models/password_reset.go
+++ models/password_reset.go
@@ -1,9 +1,14 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/AlexTLDR/WebDev/rand"
 )
 
 const (
@@ -30,9 +35,52 @@ type PasswordResetService struct {
 }
 
 func (service *PasswordResetService) Create(email string) (*PasswordReset, error) {
-	return nil, fmt.Errorf("TODO: create password reset")
+	email = strings.ToLower(email)
+	var userID int
+	row := service.DB.QueryRow(`SELECT id FROM users WHERE email = $1`, email)
+	err := row.Scan(&userID)
+	if err != nil {
+		// TODO: return a specific error when the user does not exist
+		return nil, fmt.Errorf("create password reset: %w", err)
+	}
+	bytesPerToken := service.BytesPerToken
+	if bytesPerToken < MinBytesPerToken {
+		bytesPerToken = MinBytesPerToken
+	}
+	token, err := rand.String(bytesPerToken)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	duration := service.Duration
+	if duration == 0 {
+		duration = DefaultResetDuration
+	}
+
+	pwReset := PasswordReset{
+		UserID:    userID,
+		Token:     token,
+		TokenHash: service.hash(token),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	// Insert the PasswordReset into the database
+	row = service.DB.QueryRow(`
+	INSERT INTO password_resets (user_id, token_hash, expires_at)
+	VALUES ($1, $2, $3) ON CONFLICT (user_id) DO
+	UPDATE SET token_hash = $2, expires_at = $3
+	RETURNING id;`, pwReset.UserID, pwReset.TokenHash, pwReset.ExpiresAt)
+	err = row.Scan(&pwReset.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	return &pwReset, nil
 }
 
 func (service *PasswordResetService) Consume(token string) (*User, error) {
 	return nil, fmt.Errorf("TODO: consume password reset")
+}
+
+func (service *PasswordResetService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
