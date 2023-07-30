9eceaa0afa1bdfc1ff1648da4484f0d816f0ba76
AlexTLDR <alex.badragan@protonmail.com>
2023-07-30 13:15:50 +0200

    check-your-email HTML template

--- main.go
+++ main.go
@@ -103,6 +103,7 @@ func main() {
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
 	usersC.Templates.ForgotPassword = views.Must(views.ParseFS(templates.FS, "forgot-pw.gohtml", "tailwind.gohtml"))
+	usersC.Templates.CheckYourEmail = views.Must(views.ParseFS(templates.FS, "check-your-email.gohtml", "tailwind.gohtml"))
 
 	// Routes
 
