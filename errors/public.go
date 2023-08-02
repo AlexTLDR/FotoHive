2ba0e3d42202cf3a58f81a4b7a205c5520bd7bee
AlexTLDR <alex.badragan@protonmail.com>
2023-08-02 15:48:23 +0200

    creating public errors

---
+++ errors/public.go
@@ -0,0 +1,25 @@
+package errors
+
+func Public(err error, msg string) error {
+	return publicError{
+		err: err,
+		msg: msg,
+	}
+}
+
+type publicError struct {
+	err error
+	msg string
+}
+
+func (pe publicError) Error() string {
+	return pe.err.Error()
+}
+
+func (pe publicError) Public() string {
+	return pe.msg
+}
+
+func (pe publicError) Unwrap() error {
+	return pe.err
+}
