5ac1d13e7b0158dd3f801ec9ea347177d629c265
AlexTLDR <alex.badragan@protonmail.com>
2023-08-08 11:34:17 +0200

    deleting galleries

--- models/gallery.go
+++ models/gallery.go
@@ -78,3 +78,12 @@ func (service *GalleryService) Update(gallery *Gallery) error {
 	}
 	return nil
 }
+
+func (service *GalleryService) Delete(id int) error {
+	_, err := service.DB.Exec(`
+		DELETE FROM galleries WHERE id = $1;`, id)
+	if err != nil {
+		return fmt.Errorf("delete gallery: %w", err)
+	}
+	return nil
+}
