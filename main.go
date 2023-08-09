c4d86bedc3d09aabc9a160454acb1144bf6285bd
AlexTLDR <alex.badragan@protonmail.com>
2023-08-09 12:38:10 +0200

    update gallery name handler

--- main.go
+++ main.go
@@ -140,6 +140,7 @@ func main() {
 			r.Get("/new", galleriesC.New)
 			r.Post("/", galleriesC.Create)
 			r.Get("/{id}/edit", galleriesC.Edit)
+			r.Post("/{id}", galleriesC.Update)
 		})
 	})
 	//r.Get("/users/me", usersC.CurrentUser)
