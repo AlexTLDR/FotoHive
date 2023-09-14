8e44bec36396fff05ed4fb37562e39c2c898ef6d
AlexTLDR <alex.badragan@protonmail.com>
2023-09-14 14:48:24 +0200

    DeleteImage service func

--- models/gallery.go
+++ models/gallery.go
@@ -134,6 +134,18 @@ func (service *GalleryService) Image(galleryID int, filename string) (Image, err
 	}, nil
 
 }
+
+func (service *GalleryService) DeleteImage(galleryID int, filename string) error {
+	image, err := service.Image(galleryID, filename)
+	if err != nil {
+		return fmt.Errorf("deleting image: %w", err)
+	}
+	err = os.Remove(image.Path)
+	if err != nil {
+		return fmt.Errorf("deleting image: %w", err)
+	}
+	return nil
+}
 func (service *GalleryService) extensions() []string {
 	return []string{".jpg", ".png", ".jpeg", ".gif"}
 }
