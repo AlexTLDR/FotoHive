f0e5f3138fc912f585b416f41a52b98ced63eca5
AlexTLDR <alex.badragan@protonmail.com>
2023-09-26 14:01:51 +0200

    authentichated cdm/exp/exp.go to dropbox

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -3,13 +3,61 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
 
-	"github.com/AlexTLDR/WebDev/models"
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	// testing that globbing of the image file works - replace 2 with any gallery ID
-	gs := models.GalleryService{}
-	fmt.Println(gs.Images(2))
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dropboxID := os.Getenv("DROPBOX_APP_ID")
+	dropboxSecret := os.Getenv("DROPBOX_APP_SECRET")
+	ctx := context.Background()
+	conf := &oauth2.Config{
+		ClientID:     dropboxID,
+		ClientSecret: dropboxSecret,
+		Scopes:       []string{"files.metadata.read", "files.content.read"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://www.dropbox.com/oauth2/authorize",
+			TokenURL: "https://api.dropboxapi.com/oauth2/token",
+		},
+	}
+
+	// Redirect user to consent page to ask for permission
+	// for the scopes specified above.
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	fmt.Printf("Visit the URL for the auth dialog: %v\n", url)
+	fmt.Print("Once you have the code, paste it and press enter: ")
+
+	// Use the authorization code that is pushed to the redirect
+	// URL. Exchange will do the handshake to retrieve the
+	// initial access token. The HTTP Client returned by
+	// conf.Client will refresh the token as necessary.
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatal(err)
+	}
+	tok, err := conf.Exchange(ctx, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := conf.Client(ctx, tok)
+	resp, err := client.Post("https://api.dropboxapi.com/2/files/list_folder", "application/json", strings.NewReader(`{
+		"path": ""
+	}`))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
 }
--- go.mod
+++ go.mod
@@ -11,10 +11,12 @@ require (
 	github.com/jackc/pgx/v4 v4.18.1
 	github.com/joho/godotenv v1.5.1
 	github.com/pressly/goose/v3 v3.13.4
-	golang.org/x/crypto v0.11.0
+	golang.org/x/crypto v0.13.0
+	golang.org/x/oauth2 v0.12.0
 )
 
 require (
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -23,7 +25,9 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/mail.v2 v2.3.1 // indirect
 )
