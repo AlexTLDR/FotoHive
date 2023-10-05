139a1c853ac921828de44e079a87d888db173fd0
AlexTLDR <alex.badragan@protonmail.com>
2023-10-05 13:31:06 +0200

    using errgroup

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -7,12 +7,12 @@ import (
 	"net/url"
 	"path/filepath"
 	"strconv"
-	"sync"
 
 	"github.com/AlexTLDR/WebDev/context"
 	"github.com/AlexTLDR/WebDev/errors"
 	"github.com/AlexTLDR/WebDev/models"
 	"github.com/go-chi/chi/v5"
+	"golang.org/x/sync/errgroup"
 )
 
 type Galleries struct {
@@ -237,16 +237,19 @@ func (g Galleries) ImageViaURL(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	files := r.PostForm["files"]
-	var wg sync.WaitGroup
-	wg.Add(len(files))
+
+	var eg errgroup.Group
 	for _, file := range files {
 		imageFile := file
-		go func() {
-			g.GalleryService.CreateImageViaURL(gallery.ID, imageFile)
-			wg.Done()
-		}()
+		eg.Go(func() error {
+			return g.GalleryService.CreateImageViaURL(gallery.ID, imageFile)
+		})
 	}
-	wg.Wait()
+	err = eg.Wait()
+	if err != nil {
+		http.Error(w, "Unable to download all images", http.StatusInternalServerError)
+		return
+	}
 
 	editPath := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
 	http.Redirect(w, r, editPath, http.StatusFound)
--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
